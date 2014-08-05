b81da8066f5b6eb7b2f532a8893fd493fa532801
Paul Sweeney <paul@8-bitdesign.com>
2014-08-04 22:31:38 -0700

    Building minified version

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,29 @@
 var gulp = require('gulp')
-  , coffee = 
+  , coffee = require('gulp-coffee')
+  , ngAnnotate = require('gulp-ng-annotate')
+  , uglify = require('gulp-uglify')
+  , concat = require('gulp-concat')
 
-gulp.task('default', function() {
-  gulp.src([
-    "src/index.coffee",
-    "src/**/*.coffee",
-  ])
-  .pipe(require('gulp-coffee')())
-  .pipe(require('gulp-concat')('index.js'))
-  .pipe(gulp.dest('.'))
+var files = [
+  "src/index.coffee",
+  "src/**/*.coffee",
+]
+
+gulp.task('build', function() {
+  return gulp.src(files)
+         .pipe(coffee())
+         .pipe(concat('index.js'))
+         .pipe(gulp.dest('.'))
 })
+
+gulp.task('minified', function() {
+  return gulp.src(files)
+         .pipe(coffee())
+         .pipe(ngAnnotate())
+         .pipe(uglify())
+         .pipe(concat('index.min.js'))
+         .pipe(gulp.dest('.'))
+})
+
+gulp.task('default', ['build', 'minified'])
+
